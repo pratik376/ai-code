4a2625e2ebd2f6dd32dd5141b7be37c9589ab191
pratik376 <babariyapratik208@gmail.com>
2024-09-28 18:26:50 +0530

    jergo added..

--- p1.py
+++ p1.py
@@ -8,11 +8,4 @@ else:
     print("Do your Home work")
     print("Please fuckoff")   
     print("I want to suck your boobs") 
-hungry=input("Are you hungry or not")
-if hungry == "yes":
-    print("Eat samosa")
-    print("Eat meggie")
-    print("Eat dabeli")
-else:
-    print("Do your Home work") 
-    print("Yes boy")   
+    print("jergo")
