973fbe1a0fb7321064483440c41b69a4d5f10abf
pratik376 <babariyapratik208@gmail.com>
2024-09-28 18:04:27 +0530

    done

--- p1.py
+++ p1.py
@@ -4,12 +4,4 @@ if hungry == "yes":
     print("Eat meggie")
     print("Eat dabeli")
 else:
-<<<<<<< HEAD
-    print("Do your Home work") 
-    print("Yes boy")   
-=======
-    print("Drink water and masturbate")
-    print("Do your Home work")
-    print("Please fuckoff")   
-    print("I want to suck your boobs") 
->>>>>>> water
+    print("Do your Home work")    
